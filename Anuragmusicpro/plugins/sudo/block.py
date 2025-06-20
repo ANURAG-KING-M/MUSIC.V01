5f5b66d1c9f855058341e3026fef2e9da4b24027
★彡[ᴀɴᴜʀᴀɢ ᴋɪɴɢ]彡★ <ar4592975@gmail.com>
2025-06-20 20:53:28 +0530

    ▄︻デA̷N̷U̷R̷A̷G̷P̷R̷O̷══━一

--- Anuragmusicpro/plugins/sudo/block.py
+++ Anuragmusicpro/plugins/sudo/block.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from EsproMusic import app
-from EsproMusic.misc import SUDOERS
-from EsproMusic.utils.database import add_gban_user, remove_gban_user
-from EsproMusic.utils.decorators.language import language
-from EsproMusic.utils.extraction import extract_user
+from AnuragMusicpro import app
+from AnuragMusicpro.misc import SUDOERS
+from AnuragMusicpro.utils.database import add_gban_user, remove_gban_user
+from AnuragMusicpro.utils.decorators.language import language
+from AnuragMusicpro.utils.extraction import extract_user
 from config import BANNED_USERS
 
 
