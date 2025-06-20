4a0a14a404ceb44f42801e877cf9e351af6d1578
★彡[ᴀɴᴜʀᴀɢ ᴋɪɴɢ]彡★ <ar4592975@gmail.com>
2025-06-20 20:25:26 +0530

    ▄︻デA̷N̷U̷R̷A̷G̷P̷R̷O̷══━一

--- Anuragmusicpro/core/userbot.py
+++ Anuragmusicpro/core/userbot.py
@@ -11,35 +11,35 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
-            name="EsproAss1",
+            name="AnuragMusicpro1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
             no_updates=True,
         )
         self.two = Client(
-            name="EsproAss2",
+            name="AnuragMusicpro2",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING2),
             no_updates=True,
         )
         self.three = Client(
-            name="EsproAss3",
+            name="AnuragMusicpro3",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING3),
             no_updates=True,
         )
         self.four = Client(
-            name="EsproAss4",
+            name="AnuragMusicpro4",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING4),
             no_updates=True,
         )
         self.five = Client(
-            name="EsproAss5",
+            name="AnuragMusicpro5",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING5),
