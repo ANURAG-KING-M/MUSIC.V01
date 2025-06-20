f56cdde0e2130842fde5089fd80a761e5f3ff625
★彡[ᴀɴᴜʀᴀɢ ᴋɪɴɢ]彡★ <ar4592975@gmail.com>
2025-06-20 21:11:32 +0530

    ▄︻デA̷N̷U̷R̷A̷G̷P̷R̷O̷══━一

--- Anuragmusicpro/utils/sys.py
+++ Anuragmusicpro/utils/sys.py
@@ -2,8 +2,8 @@ import time
 
 import psutil
 
-from EsproMusic.misc import _boot_
-from EsproMusic.utils.formatters import get_readable_time
+from AnuragMusicpro.misc import _boot_
+from AnuragMusicpro.utils.formatters import get_readable_time
 
 
 async def bot_sys_stats():
