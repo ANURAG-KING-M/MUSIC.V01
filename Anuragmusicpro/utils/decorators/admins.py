411374dfeb216206886ae753561b86f6145ba0b2
★彡[ᴀɴᴜʀᴀɢ ᴋɪɴɢ]彡★ <ar4592975@gmail.com>
2025-06-20 21:02:17 +0530

    ▄︻デA̷N̷U̷R̷A̷G̷P̷R̷O̷══━一

--- Anuragmusicpro/utils/decorators/admins.py
+++ Anuragmusicpro/utils/decorators/admins.py
@@ -1,9 +1,9 @@
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from EsproMusic import app
-from EsproMusic.misc import SUDOERS, db
-from EsproMusic.utils.database import (
+from AnuragMusicpro import app
+from AnuragMusicpro.misc import SUDOERS, db
+from AnuragMusicpro.utils.database import (
     get_authuser_names,
     get_cmode,
     get_lang,
