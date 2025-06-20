2e45c74e51d437886d037b301bc6c730cf5553a3
★彡[ᴀɴᴜʀᴀɢ ᴋɪɴɢ]彡★ <ar4592975@gmail.com>
2025-06-20 21:23:52 +0530

    ▄︻デA̷N̷U̷R̷A̷G̷P̷R̷O̷══━一

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 5000))
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", "7745362209"))
+OWNER_ID = int(getenv("OWNER_ID", None))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -31,7 +31,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SULTANBHAI87/ANURAGPROMUSIC",
+    "https://github.com/ANURAG-KING-M/MUSIC.V1",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
